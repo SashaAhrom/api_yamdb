4146da8fe4c1606d924ac5d9588684c0ed9e5b7a
Рябов Игорь Владимирович <S:\soft\The Bat!\Mail>
2022-03-02 22:26:07 +0300

    Deleted unused permissions

--- api_yamdb/users/permissions.py
+++ api_yamdb/users/permissions.py
@@ -1,6 +1,10 @@
 from rest_framework import permissions
 
+
 class IsAdmin(permissions.BasePermission):
+    """Custom permission for users with admin role."""
+
+    message = 'Доступ только для администраторов и выше!'
 
     def has_permission(self, request, view):
         if request.user.is_authenticated:
@@ -9,17 +13,3 @@ class IsAdmin(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
         return request.user.role == 'admin' or request.user.is_superuser
-
-
-class IsModerator(permissions.BasePermission):
-
-    def has_permission(self, request, view):
-        return request.user.is_staff and request.user.role == 'moderator'
-
-
-class IsAuthorOrAdmin(permissions.BasePermission):
-
-    def has_object_permission(self, request, view, obj):
-        return (obj.author == request.user
-                or (request.user.is_staff and request.user.role == 'admin')
-        )
